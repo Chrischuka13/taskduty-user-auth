5f0428c1a45344637391dd932d88ecf1109ce398
Chrischuka13 <divineprovidence30@gmail.com>
2026-03-14 12:23:32 +0100

    samesite set to none from strict

--- routes/auth.js
+++ routes/auth.js
@@ -62,7 +62,7 @@ router.post("/login", async(req, res) =>{
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "strict",
+            sameSite: "none",
         })
 
         res.status(200).json({
